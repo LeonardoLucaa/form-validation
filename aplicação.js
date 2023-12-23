fa8dfbca32b4dacdb22538e443a7e63578c406ea
LeonardoLucaa <leonardolucab@gmail.com>
2023-12-23 15:32:30 -0300

    web-development-2

---
+++ aplicação.js
@@ -0,0 +1,44 @@
+const Livro = {
+    title: "GoosebumpsHorrorland19",
+    author: "StineRL",
+    totalOfPages: "104",
+    currentpage: "1",
+    toggleOnOff: function() {
+        if(Livro.title) {
+            console.log("desligar");
+        } else {
+            console.log("ligar");
+        }
+        Livro.title = !Livro.title;
+
+        if(Livro.author) {
+            console.log("desligar");
+        } else {
+            console.log("ligar");
+        }
+        Livro.author = !Livro.author;
+
+        if(Livro.totalOfPages) {
+            console.log("desligar");
+        } else {
+            console.log("ligar");
+        }
+        Livro.totalOfPages = !Livro.totalOfPages;
+
+    var currentpage = ["1"];
+    alert("A página atual é a " + página[0]);
+    setAttribute(currentpage)
+  
+function percentage_2() {
+  
+    var totalOfPages = document.getElementById("num1").value;
+      
+    var currentpage = document.getElementById("num2").value;
+    document.getElementById("value2")
+        .value = (num1 * 100) / num2 + "%";
+
+        var arrayLivro = ["GoosebumpsHorrorland19", "StineRL", "104", "1"];
+        console.log("title: " + arrayLivro[0] + " " + arrayLivro[1]);
+        console.log("author: " + arrayLivro[2] + " " + "totalOfPages: " + arrayLivro[3]);
+        }
+    }
